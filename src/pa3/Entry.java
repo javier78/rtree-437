a2a35322d11bc89e241799291d8ef9ef91ee0439
Javier Martinez <javier.martinez1024@gmail.com>
2014-11-28 22:53:48 -0500

    Minor changes.

--- src/pa3/Entry.java
+++ src/pa3/Entry.java
@@ -2,16 +2,37 @@ package pa3;
 
 import java.io.Serializable;
 
-public abstract class Entry implements Serializable
+public class Entry implements Serializable
 {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1864185831524829527L;
 	Rectangle mbr = new Rectangle(0,0,0,0);
-	Tuple[] tuples = new Tuple[10]; //TODO: Calculate correct number of tuples!!!
+	Rectangle[] containedRectangles; //TODO: Calculate correct number of rectangles!!!
 	
-	public abstract Rectangle calculateMBR();
+	public Rectangle calculateMBR()
+	{
+		int minX = Integer.MAX_VALUE;
+		int maxX = 0;
+		int minY = Integer.MAX_VALUE;
+		int maxY = 0;
+		for(Rectangle r : rectangles)
+		{
+			minX = r.getX1() < minX ? r.getX1() : minX;
+			minX = r.getX2() < minX ? r.getX2() : minX;
+			minY = r.getY1() < minY ? r.getY1() : minY;
+			minY = r.getY2() < minY ? r.getY2() : minY;
+			
+			maxX = r.getX1() > maxX ? r.getX1() : maxX;
+			maxX = r.getX2() > maxX ? r.getX2() : maxX;
+			maxY = r.getY1() > maxY ? r.getY1() : maxY;
+			maxY = r.getY2() > maxY ? r.getY2() : maxY;
+		}
+		
+		mbr = new Rectangle(minX, minY, maxX, maxY);
+		return mbr;
+	}
 	
 	public Rectangle getMBR()
 	{
--- src/pa3/IndexNode.java
+++ src/pa3/IndexNode.java
@@ -9,7 +9,7 @@ public class IndexNode extends Node
 	NodeReference[] refs;
 	Rectangle[] mbr;
 	private final int MAX_ENTRIES = 204;	//Found this value by summing the size of all fields in an index entry and multiplying each size by 2d, where d is the order of the tree.
-	//size of a NodeReference = 4 bytes, size of a Rectangle is 16 bytes (contains 4 integers).
+	//size of a NodeReference = 4 bytes, size of a Rectangle is 16 bytes (contains 4 integers). Figure out how many entries also fit!!
 	//So: 2d * (4 * 4) + 2d * (4) <= 4096
 	//==>32d + 8d <= 4096
 	//==>40d <= 4096
